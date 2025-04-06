9e5c632cdb47f20913967b05de30659e0f6b423a
Alexander Komla <kunalexander360@gmail.com>
2025-04-06 08:02:04 +0000

    Cutting and padding

--- index.py
+++ index.py
@@ -6,11 +6,17 @@ from torchaudio.transforms import MelSpectrogram
 import os  
 
 class UrbanSoundDataset(Dataset): 
-    def __init__(self, annotations_file, audio_dir, transformation, target_sample_rate): 
+    def __init__(self, 
+                 annotations_file, 
+                 audio_dir, 
+                 transformation, 
+                 target_sample_rate, 
+                 num_samples): 
         self.annotations = pd.read_csv(annotations_file)
         self.audio_dir = audio_dir 
         self.transformation = transformation
         self.target_sample_rate = target_sample_rate
+        self.num_samples = num_samples
 
     def __len__(self): 
         return len(self.annotations) 
@@ -22,10 +28,26 @@ class UrbanSoundDataset(Dataset):
         signal, sr = torchaudio.load(audio_sample_path, backend="soundfile") 
         # signal -> (num_channels, samples) -> (2, 1600)
         signal = self._resample_if_necessary(signal, sr)
-        signal = self._mix_down_if_necessary(signal) #(1, 1600) 
+        signal = self._mix_down_if_necessary(signal) #(1, 1600)  
+        signal = self._right_pad_if_necessary(signal)
+        signal = self._cut_if_necessary(signal)
         signal = self.transformation(signal)
         return signal, label 
     
+    def _cut_if_necessary(self, signal): 
+        # signal -> (Tensor) -> (1, num_samples)
+        if signal.shape[1] > self.num_samples: 
+            signal = signal[:, :self.num_samples] 
+        return signal 
+    
+    def _right_pad_if_necessary(self, signal): 
+        signal_length = signal.shape[1] 
+        if signal_length < self.num_samples: 
+            # [1, 1, 1] -> [1, 1, 1, 0, 0] 
+            num_missing_samples = self.num_samples - signal_length 
+            last_dimension_padding = (0, num_missing_samples)
+            signal = torch.nn.functional.pad(signal, last_dimension_padding)
+        return signal
     def _get_audio_sample_path(self, index): 
         fold = f"fold{self.annotations.iloc[index, 5]}" 
         path = os.path.join(self.audio_dir, fold, self.annotations.iloc[index, 0]) 
@@ -48,17 +70,22 @@ class UrbanSoundDataset(Dataset):
 if __name__ == "__main__": 
     ANNOTATIONS_FILE = "./data/metadata/UrbanSound8K.csv"
     AUDIO_DIR = "./data/audio" 
-    SAMPLE_RATE = 16000 
+    SAMPLE_RATE = 22050
+    NUM_SAMPLES = 22050
     mel_spectrogram = torchaudio.transforms.MelSpectrogram(
         sample_rate=SAMPLE_RATE, 
         n_fft= 1024, 
         hop_length=512, 
         n_mels=64
     )
-    usd = UrbanSoundDataset(ANNOTATIONS_FILE, AUDIO_DIR, mel_spectrogram, SAMPLE_RATE)
+    usd = UrbanSoundDataset(ANNOTATIONS_FILE, 
+                            AUDIO_DIR, 
+                            mel_spectrogram, 
+                            SAMPLE_RATE, 
+                            NUM_SAMPLES)
     print(f"There are {len(usd)} samples in the dataset.") 
 
-    signal, label = usd[1] 
+    signal, label = usd[10] 
     print(signal.shape)
     print(label)
 
